0ed33d852608a9fce5dc95c161e7fb05cb6bd867
Dan Kapner <danielk@alleninstitute.org>
2019-09-19 16:08:57 -0700

    updating name

--- setup.py
+++ setup.py
@@ -29,12 +29,12 @@ with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
 
-setup(name="nway_recover",
+setup(name="ophys_nway_matching",
       description=("building up a repo from the ophys_ophys_nway "
                    "running in LIMS, Aug 2019"),
       author="Daniel Kapner, original code Fuhui Long",
       author_email='danielk@alleninstitute.org',
-      url="https://github.com/AllenInstitute/nway_recover",
+      url="https://github.com/AllenInstitute/ophys_nway_matching",
       packages=find_packages(),
       setup_requires=['setuptools_scm'],
       install_requires=required,
