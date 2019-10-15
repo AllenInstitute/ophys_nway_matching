5b56bf74c1230bd1bae555f0561d70e69dfd6ceb
Dan Kapner <danielk@alleninstitute.org>
2019-10-15 15:05:49 -0700

    using scm version

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ with open('requirements.txt', 'r') as f:
 
 
 setup(name="ophys_nway_matching",
+      use_scm_version=True,
       description=("building up a repo from the ophys_ophys_nway "
                    "running in LIMS, Aug 2019"),
       author="Daniel Kapner, original code Fuhui Long",
