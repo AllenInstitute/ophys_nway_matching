57725f514cd351fed3dcd1e0982310f7ef096a41
Dan <danielk@alleninstitute.org>
2020-08-27 17:57:18 -0700

    parcels out registration steps

--- nway/pairwise_matching.py
+++ nway/pairwise_matching.py
@@ -399,6 +399,134 @@ def transform_mask(moving, dst_shape, tform):
     return transformed_3d
 
 
+def register_image_pair(img_fixed, img_moving, maxCount,
+                        epsilon, motion_type, gaussFiltSize):
+    """find the transform that registers two images
+
+    Parameters
+    ----------
+    img_fixed : numpy.ndarray
+        CV_8U (uint8) or CV_32F (float32) fixed image
+    img_moving : numpy.ndarray
+        CV_8U (uint8) or CV_32F (float32) moving image
+    maxCount : int
+        passed as maxCount to opencv termination criteria
+    epsilon : float
+        passed as epsilon to opencv termination criteria
+    motion_type : str
+        one of the 4 possible motion types for opencv findTransformECC
+        see the dictionary cvmotion below for the 4 possible values
+    gaussFiltSize : int
+        passed to opencv findTransformECC(). An optional value
+        indicating size of gaussian blur filter
+
+    Returns
+    -------
+    tform : :class:`numpy.ndarry`
+        3 x 3 transformation matrix
+
+    """
+
+    cvmotion = {
+            "MOTION_TRANSLATION": cv2.MOTION_TRANSLATION,
+            "MOTION_EUCLIDEAN": cv2.MOTION_EUCLIDEAN,
+            "MOTION_AFFINE": cv2.MOTION_AFFINE,
+            "MOTION_HOMOGRAPHY": cv2.MOTION_HOMOGRAPHY}
+
+    # Define termination criteria
+    criteria = (
+            cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
+            maxCount,
+            epsilon)
+
+    warp_matrix = np.eye(2, 3, dtype=np.float32)
+    if motion_type == 'MOTION_HOMOGRAPHY':
+        warp_matrix = np.eye(3, 3, dtype=np.float32)
+
+    try:
+        # Run the ECC algorithm. The results are stored in warp_matrix.
+        ccval, tform = cv2.findTransformECC(
+                img_fixed,
+                img_moving,
+                warp_matrix,
+                cvmotion[motion_type],
+                criteria,
+                None,
+                gaussFiltSize)
+    except cv2.error:
+        logger.error("failed to align images.")
+        raise
+
+    # not all the transforms output 3 x 3
+    if tform.shape == (2, 3):
+        tform = np.vstack((tform, [0, 0, 1]))
+
+    return tform
+
+
+def contrast_adjust(image, CLAHE_grid, CLAHE_clip):
+    """contrast adjust images
+
+    Parameters
+    ----------
+    image: numpy.ndaray
+        image to pre-process. CV_8UC1 (uint8) or CV_16UC1 (uint16)
+    CLAHE_grid : int
+        passed as tileGridSize to cv2.createCLAHE. If -1, skipped
+    CLAHE_clip : float
+        passed as clipLimit to cv2.createCLAHE
+
+    Returns
+    -------
+    image: numpy.narray
+        contrast-adjusted image
+
+    """
+    if CLAHE_grid != -1:
+        clahe = cv2.createCLAHE(
+            clipLimit=CLAHE_clip,
+            tileGridSize=(CLAHE_grid, CLAHE_grid))
+        image = clahe.apply(image)
+    return image
+
+
+def warp_image(image, tform, motion_type, dst_shape):
+    """
+
+    Parameters
+    ----------
+    image: numpy.ndarray
+        image to warp. Assumes uint8
+    tform: numpy.ndarray
+        3x3 transformation matrix
+    motion_type: str
+        kind of motion. See function `register_image_pair` for possible
+        values
+    dst_shape: tuple
+        desired shape of warped image
+
+    Returns
+    -------
+    image: numpy.ndarray
+        warped image, forced to uint8
+
+    """
+    if motion_type == 'MOTION_HOMOGRAPHY':
+        warp = cv2.warpPerspective
+    else:
+        warp = cv2.warpAffine
+        if tform.shape[0] == 3:
+            tform = tform[0:2]
+
+    image = warp(
+            image,
+            tform,
+            dst_shape[::-1],
+            flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP).astype(np.uint8)
+
+    return image
+
+
 def register_intensity_images(
         img_path_fixed, img_path_moving, maxCount,
         epsilon, motion_type, gaussFiltSize, CLAHE_grid, CLAHE_clip):
@@ -433,65 +561,26 @@ def register_intensity_images(
 
     """
 
-    cvmotion = {
-            "MOTION_TRANSLATION": cv2.MOTION_TRANSLATION,
-            "MOTION_EUCLIDEAN": cv2.MOTION_EUCLIDEAN,
-            "MOTION_AFFINE": cv2.MOTION_AFFINE,
-            "MOTION_HOMOGRAPHY": cv2.MOTION_HOMOGRAPHY}
-
     # read average intensity images
     with PIL.Image.open(img_path_fixed) as im:
         img_fixed = np.array(im)
     with PIL.Image.open(img_path_moving) as im:
         img_moving = np.array(im)
 
-    if CLAHE_grid != -1:
-        clahe = cv2.createCLAHE(
-            clipLimit=CLAHE_clip,
-            tileGridSize=(CLAHE_grid, CLAHE_grid))
-        img_fixed = clahe.apply(img_fixed)
-        img_moving = clahe.apply(img_moving)
+    # perform contrast adjustment
+    img_fixed = contrast_adjust(img_fixed, CLAHE_grid, CLAHE_clip)
+    img_moving = contrast_adjust(img_moving, CLAHE_grid, CLAHE_clip)
 
-    # Define termination criteria
-    criteria = (
-            cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
-            maxCount,
-            epsilon)
+    # find the transform
+    tform = register_image_pair(img_fixed, img_moving, maxCount,
+                                epsilon, motion_type, gaussFiltSize)
 
-    warp_matrix = np.eye(2, 3, dtype=np.float32)
-    if motion_type == 'MOTION_HOMOGRAPHY':
-        warp_matrix = np.eye(3, 3, dtype=np.float32)
-
-    try:
-        # Run the ECC algorithm. The results are stored in warp_matrix.
-        ccval, tform = cv2.findTransformECC(
-                img_fixed,
-                img_moving,
-                warp_matrix,
-                cvmotion[motion_type],
-                criteria,
-                None,
-                gaussFiltSize)
-    except cv2.error:
-        logger.error("failed to align images {} and {}".format(
-            img_path_fixed,
-            img_path_moving))
-        raise
-
-    if motion_type == 'MOTION_HOMOGRAPHY':
-        warp = cv2.warpPerspective
-    else:
-        warp = cv2.warpAffine
-
-    img_moving_warped = warp(
+    # warp the moving image
+    img_moving_warped = warp_image(
             img_moving,
             tform,
-            img_fixed.shape[::-1],
-            flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP).astype(np.uint8)
-
-    # if affine only, output full 3x3
-    if tform.shape == (2, 3):
-        tform = np.vstack((tform, [0, 0, 1]))
+            motion_type,
+            img_fixed.shape)
 
     return tform, img_moving_warped
 
