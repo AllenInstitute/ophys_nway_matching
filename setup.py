22858aea0426310b1e7286436b209fd116abe365
Dan <danielk@alleninstitute.org>
2020-09-12 07:38:59 -0700

    removes test_requirements, setup.py test, pins opencv

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
 from setuptools import setup, find_packages
-import sys
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ""
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import shlex
-        import pytest
-        self.pytest_args += " --cov=nway --cov-report html "\
-                            "--cov-report xml "\
-                            "--junitxml=test-reports/test.xml"
-
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
-
-
-with open('test_requirements.txt', 'r') as f:
-    test_required = f.read().splitlines()
 
 
 with open('requirements.txt', 'r') as f:
@@ -39,6 +14,4 @@ setup(name="ophys_nway_matching",
       url="https://github.com/AllenInstitute/ophys_nway_matching",
       packages=find_packages(),
       setup_requires=['setuptools_scm'],
-      install_requires=required,
-      tests_require=test_required,
-      cmdclass={'test': PyTest})
+      install_requires=required)
