c9eb5185adfb0a3d85da5f5c67e7f2752afdd132
Dan <danielk@alleninstitute.org>
2021-02-02 06:26:00 -0800

    restructure dependency management, for optional dependency for LIMS

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
-
-with open('requirements.txt', 'r') as f:
-    required = f.read().splitlines()
-
-
-setup(name="ophys_nway_matching",
-      use_scm_version=True,
-      description=("building up a repo from the ophys_ophys_nway "
-                   "running in LIMS, Aug 2019"),
-      author="Daniel Kapner, original code Fuhui Long",
-      author_email='danielk@alleninstitute.org',
-      url="https://github.com/AllenInstitute/ophys_nway_matching",
-      package_dir={"": "src"},
-      packages=find_packages(where="src"),
-      setup_requires=['setuptools_scm'],
-      install_requires=required)
+if __name__ == "__main__":
+    setuptools.setup()
